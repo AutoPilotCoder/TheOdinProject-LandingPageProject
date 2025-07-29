b964aba971af40f00843ae37baab8a817f3080a7
AutoPilotCoder <Haodeveloper3@gmail.com>
2025-07-29 20:31:47 +0800

    Js 4 sample functions

--- javascript.js
+++ javascript.js
@@ -1,2 +1,15 @@
-let one = 1
-console.log(one)
+function add7(num) {
+    return parseInt(num) + 7
+}
+
+function multiply(n1, n2) {
+    return parseInt(n1) * parseInt(n2)
+}
+
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+}
+
+function lastLetter(str) {
+    return str.slice(str.length - 1)
+}
